e172366ee3c50e2917891e615f739255604caea6
maliByatzes <malib2027@gmail.com>
2023-11-25 01:00:38 +0200

    add `echo` like command

--- shout.c
+++ shout.c
@@ -1,6 +1,17 @@
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  printf("Hello shout!\n");
-  return 0;
+int main(int argc, char *argv[]) {
+  assert(argc > 0);
+  if (argc < 2) {
+    printf("Not enough arguments. Please run: %s [message]\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  for (int i = 1; i <= (argc - 1); ++i)
+    printf("%s ", argv[i]);
+  printf("\n");
+
+  return EXIT_SUCCESS;
 }
